211d430c5e9a396092cb8fc68ff112959a35280f
Carrollyn Catanzaro <endyrekkyrts@yahoo.com>
2018-02-14 17:22:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  return kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift("Bob")
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  var newKittens = ("Broom")
+  return newKittens
+}
+
+function removeLastKitten() {
+  var newKittens = kittens.slice(kittens.length -1)
+  return newKittens
+  
+}
+
+function removeFirstKitten() {
+  var newKittens = kittens.slice(kittens)
+  return newKittens
+}
